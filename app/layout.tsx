e87aee085780ba1194ac4bd5bdb80504d754d7e1
Arnold <aizkalns8@gmail.com>
2023-06-29 14:43:38 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,77 @@
+import { Inter } from '@next/font/google'
+import LocalFont from '@next/font/local'
+import { Metadata } from 'next'
+import '../global.css'
+import { Analytics } from './components/analytics'
+
+export const metadata: Metadata = {
+	title: {
+		default: 'chronark.com',
+		template: '%s | chronark.com',
+	},
+	description: 'Software engineer at upstash.com and founder of planetfall.io',
+	openGraph: {
+		title: 'chronark.com',
+		description:
+			'Software engineer at upstash.com and founder of planetfall.io',
+		url: 'https://chronark.com',
+		siteName: 'chronark.com',
+		images: [
+			{
+				url: 'https://chronark.com/og.png',
+				width: 1920,
+				height: 1080,
+			},
+		],
+		locale: 'en-US',
+		type: 'website',
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+	twitter: {
+		title: 'Chronark',
+		card: 'summary_large_image',
+	},
+	icons: {
+		shortcut: '/favicon.png',
+	},
+}
+const inter = Inter({
+	subsets: ['latin'],
+	variable: '--font-inter',
+})
+
+const audio = LocalFont({
+	src: '../public/fonts/Audiowide-Regular.ttf',
+	variable: '--font-audio',
+})
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	return (
+		<html lang='en' className={[inter.variable, audio.variable].join(' ')}>
+			<head>
+				<Analytics />
+			</head>
+			<body
+				className={`bg-gray-700 ${
+					process.env.NODE_ENV === 'development' ? 'debug-screens' : undefined
+				}`}
+			>
+				{children}
+			</body>
+		</html>
+	)
+}
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import { withContentlayer } from 'next-contentlayer'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+}
+
+export default withContentlayer(nextConfig)
